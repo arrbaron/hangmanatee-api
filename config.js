12333eb52acda8895fb46e41761875a56c45c3c2
Roy Baron <baron.roy@gmail.com>
2018-01-04 13:27:19 -0500

    add mongoose dotenv morgan and bodyparser dependencies

---
+++ config.js
@@ -0,0 +1,8 @@
+exports.DATABASE_URL =
+  process.env.DATABASE_URL ||
+  global.DATABASE_URL ||
+  `mongodb://localhost/hangmanatee`;
+
+exports.PORT = process.env.PORT || 8080;
+exports.JWT_SECRET = process.env.JWT_SECRET || "pizzapizzabananabanana";
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || `7d`;
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const { PORT, DATABASE_URL } = require("./config");
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+// app.use(express.static("public"));
+app.use(morgan("common"));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 app.get("/api/*", (req, res) => {
   res.json({ok: true});
