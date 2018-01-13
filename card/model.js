2aa032edb9066a845f86d98b2b74a18908be9788
Roy Baron <baron.roy@gmail.com>
2018-01-13 13:50:48 -0500

    clean up endpoints

--- card/model.js
+++ card/model.js
@@ -5,7 +5,8 @@ mongoose.Promise = global.Promise;
 const CardSchema = mongoose.Schema({
   term: String,
   definition: String,
-  showEdit: Boolean
+  showEdit: Boolean,
+  showTerm: Boolean
 });
 
 const Card = mongoose.model("Card", CardSchema);
